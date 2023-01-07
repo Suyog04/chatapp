459daec152b0109916a325dbf10920a94d9fbfb9
Suyog04 <72078239+Suyog04@users.noreply.github.com>
2023-01-07 18:11:15 +0545

    committed

--- letsconnect/src/App.js
+++ letsconnect/src/App.js
@@ -1,14 +1,43 @@
-import logo from './logo.svg';
+import React from `react` ;
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+firebase.initializeApp({
+  apiKey: "AIzaSyBNPfdGC3VpXZs0md9Q5w1V-BtLb6XvEuk",
+  authDomain: "letsconnect-bf285.firebaseapp.com",
+  projectId: "letsconnect-bf285",
+  storageBucket: "letsconnect-bf285.appspot.com",
+  messagingSenderId: "958357147280",
+  appId: "1:958357147280:web:302318a90e6e62ae655215",
+  measurementId: "G-G92WG9GYR8"
+]}
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header>
+        <h1> letsconnect</h1>
+        <SignOut />
+
+        <section>
+          {
+            user ? <chatRoom/>: <SignIn />
+          }
+        </section>
       </header>
     </div>
   );
 }
+
+function SignIn
 
 export default App;
